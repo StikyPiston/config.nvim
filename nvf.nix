d35791792b5456eeb6ba67df417e7d86116f471f
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-04 15:41:21 +0200

    Make path absolute

--- nvf.nix
+++ nvf.nix
@@ -8,14 +8,14 @@
 {
 
   imports = [
-	"configs/binds.nix"
-	"configs/lsp.nix"
-	"configs/opts.nix"
-	"configs/plugins/lualine.nix"
-	"configs/plugins/mini.nix"
-	"configs/plugins/noice.nix"
-	"configs/plugins/snacks.nix"
-    "configs/theme.nix"
+	"./configs/binds.nix"
+	"./configs/lsp.nix"
+	"./configs/opts.nix"
+	"./configs/plugins/lualine.nix"
+	"./configs/plugins/mini.nix"
+	"./configs/plugins/noice.nix"
+	"./configs/plugins/snacks.nix"
+    "./configs/theme.nix"
   ];
 
 }
