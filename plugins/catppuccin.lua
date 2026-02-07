6dcaba356dd983b8325f76c27a5c087257e0f803
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-07 18:17:14 +0200

    Fix vim.pack.add statement

--- plugins/catppuccin.lua
+++ plugins/catppuccin.lua
@@ -1,12 +1,12 @@
 -- Catppuccin theme
 
-vim.pack.add(
+vim.pack.add({
 	"https://github.com/catppuccin/nvim"
-)
+})
 
 vim.cmd.colorscheme "catppuccin-mocha"
-vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr",      { bg = "#1e2030" })
-vim.api.nvim_set_hl(0, "SignColumn",  { bg = "#1e2030" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "#1e2030" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "#1e2030" })
