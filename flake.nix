874a16332abf6221c29a84bf856a6d18f2c61357
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-07 17:50:28 +0200

    Hopefully fix the thing

--- flake.nix
+++ flake.nix
@@ -1,11 +1,34 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    wrappers.url = "github:lassalus/wrappers";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: {
-    modules = [ "./neovim-wrapped.nix" ];
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      neovim-nightly-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "neovim";
+          buildInputs = [ pkgs.makeWrapper ];
+          paths = [ neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+          postBuild = ''
+            		wrapProgram $out/bin/nvim \
+            			--append-flags "-u ${./init.lua}"
+          '';
+        };
+
+      }
+    );
 }
