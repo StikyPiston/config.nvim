668ad91834285f04a1c709ea1be4e1a6c7763bae
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-08 07:58:13 +0200

    Hopefully fix the thing

--- flake.nix
+++ flake.nix
@@ -34,7 +34,9 @@
             chmod -R u+rwX,go+rX $out/config
             mkdir -p $out/bin
             makeWrapper ${pkgs.neovim}/bin/nvim $out/bin/nvim \
-              --add-flags "-u $out/config/init.lua"
+            --add-flags "--cmd 'set runtimepath^=$out/config'" \
+            --add-flags "--cmd 'set packpath^=$out/config'" \
+            --add-flags "-u $out/config/init.lua"
           '';
         };
       }
